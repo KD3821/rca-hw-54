979db5b750df7e6ff7f0c0212d523995141eb078
KD3821 <korden3821@gmail.com>
2024-11-27 22:04:18 +0300

    feat: add websocket conn & update js script

--- app/main.py
+++ app/main.py
@@ -1,25 +1,66 @@
-import asyncio
-import time
+import uuid
+from pathlib import Path
 
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse, StreamingResponse
+import uvicorn
+from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect, Query
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
 
+directory = Path(__file__).parent.parent
 
-async def event_stream():
-    while True:
-        await asyncio.sleep(1)
-        yield f"data: Current time is {time.ctime()}\n\n"
+app.mount("/static", StaticFiles(directory=f"{directory}/static"), name="static")
+
+templates = Jinja2Templates(directory=f"{directory}/templates")
 
 
-@app.get("/events")
-async def events():
-    return StreamingResponse(event_stream(), media_type="text/event-stream")
+async def event_stream(data: int | str):
+    yield
+    while data:
+        if data == "STOP":
+            break
+        data = yield f"⏱: {data} sec"
+    yield "🚀"
+
+
+@app.websocket("/events")
+async def events(websocket: WebSocket, session_id: str = Query()):
+    await websocket.accept(
+        headers=[("session_id".encode(), session_id.encode())]
+    )
+    try:
+        counter = 5
+        gen = event_stream(counter)
+        await gen.asend(None)
+        send_text = await gen.__anext__()
+        while counter >= 0:
+            await websocket.send_json({"text": send_text})
+            received_text = await websocket.receive_json()
+            print(received_text)
+            counter -= 1
+            send_text = await gen.asend(counter)
+        await gen.asend("STOP")
+    except StopAsyncIteration:
+        pass
+    except WebSocketDisconnect:
+        await websocket.close()
 
 
 @app.get("/")
-async def index():
-    html_content = 'test'
-    return HTMLResponse(content=html_content)
+async def index(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        headers={"session_id": str(uuid.uuid4())},
+    )
+
+
+if __name__ == '__main__':
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+    )
--- static/code.js
+++ static/code.js
@@ -1,7 +1,64 @@
-const eventSource = new EventSource("/events");
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
 
-eventSource.onmessage = function(event) {
-    const newElement = document.createElement("div");
-    newElement.innerText = event.data;
-    document.getElementById("events").appendChild(newElement);
-};
+
+async function receiveData(socket, element) {
+    socket.onmessage = (event) => {
+        let data = JSON.parse(event.data);
+        console.log('data:', data);
+        element.innerText = data.text;
+    }
+    if (element.innerText === "🚀") {
+        socket.close();
+        console.log("Connection closed for socket: ", socket);
+        let altitude = 1;
+        do {
+            await setAltitude(element, altitude*10);
+            altitude++;
+        } while (altitude <= 20);
+    } else {
+        setTimeout(() => sendData(socket, element, `Confirmed: ${element.innerText}`), 1000);
+    }
+}
+
+
+async function sendData(socket, element, text) {
+    socket.send(JSON.stringify(text));
+    await receiveData(socket, element);
+}
+
+
+async function setAltitude(element, altitude) {
+    let boom = document.getElementById("boom");
+    if (altitude === 10) {
+        boom.style.visibility = "visible";
+    } else {
+        boom.style.visibility = "hidden";
+    }
+    await sleep(300);
+    if (altitude === 150) {
+        element.innerText = "💥";
+    } else if (altitude === 200) {
+        element.style.visibility = "hidden";
+    } else if (altitude > 150) {
+        element.innerText = "💨";
+    }
+    element.style.marginLeft = `${altitude}px`;
+}
+
+
+async function getWebsocketConn() {
+    let element = document.getElementById("events");
+    let req = new XMLHttpRequest();
+    req.open("GET", document.location, true);
+    req.send(null);
+    req.onload = () => {
+        const sessionId = req.getResponseHeader("Session_id");
+        const eventSocket = new WebSocket(`ws://0.0.0.0:8000/events?session_id=${sessionId}`);
+        eventSocket.onopen = () => {
+            console.log(`WebSocket connection ${sessionId} established.`);
+        };
+        receiveData(eventSocket, element);
+    }
+}
